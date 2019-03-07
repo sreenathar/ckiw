f05337178dafbf880b216fa5a75ca083d9ff020a
sreenathar <sreenath.ar2011@gmail.com>
2019-03-07 19:59:36 +0530

    2

--- wp-login.php
+++ wp-login.php
@@ -9,11 +9,11 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 	<title>Log In &lsaquo; Citizenship &#8212; WordPress</title>
 	<link rel='dns-prefetch' href='//s.w.org' />
-<link rel='stylesheet' id='dashicons-css'  href='/https://sreenathar.github.io/ckiw/wp-includes/css/dashicons.min.css?ver=5.1' type='text/css' media='all' />
-<link rel='stylesheet' id='buttons-css'  href='/https://sreenathar.github.io/ckiw/wp-includes/css/buttons.min.css?ver=5.1' type='text/css' media='all' />
-<link rel='stylesheet' id='forms-css'  href='/https://sreenathar.github.io/ckiw/wp-admin/css/forms.min.css?ver=5.1' type='text/css' media='all' />
-<link rel='stylesheet' id='l10n-css'  href='/https://sreenathar.github.io/ckiw/wp-admin/css/l10n.min.css?ver=5.1' type='text/css' media='all' />
-<link rel='stylesheet' id='login-css'  href='/https://sreenathar.github.io/ckiw/wp-admin/css/login.min.css?ver=5.1' type='text/css' media='all' />
+<link rel='stylesheet' id='dashicons-css'  href='https://sreenathar.github.io/ckiw/wp-includes/css/dashicons.min.css?ver=5.1' type='text/css' media='all' />
+<link rel='stylesheet' id='buttons-css'  href='https://sreenathar.github.io/ckiw/wp-includes/css/buttons.min.css?ver=5.1' type='text/css' media='all' />
+<link rel='stylesheet' id='forms-css'  href='https://sreenathar.github.io/ckiw/wp-admin/css/forms.min.css?ver=5.1' type='text/css' media='all' />
+<link rel='stylesheet' id='l10n-css'  href='https://sreenathar.github.io/ckiw/wp-admin/css/l10n.min.css?ver=5.1' type='text/css' media='all' />
+<link rel='stylesheet' id='login-css'  href='https://sreenathar.github.io/ckiw/wp-admin/css/login.min.css?ver=5.1' type='text/css' media='all' />
 	<meta name='robots' content='noindex,noarchive' />
 	<meta name='referrer' content='strict-origin-when-cross-origin' />
 		<meta name="viewport" content="width=device-width" />
@@ -22,7 +22,7 @@
 		<div id="login">
 		<h1><a href="https://wordpress.org/" title="Powered by WordPress">Powered by WordPress</a></h1>
 	
-	<form name="loginform" id="loginform" action="/https://sreenathar.github.io/ckiw/wp-login.php" method="post">
+	<form name="loginform" id="loginform" action="https://sreenathar.github.io/ckiw/wp-login.php" method="post">
 	<p>
 		<label for="user_login">Username or Email Address<br />
 		<input type="text" name="log" id="user_login" class="input" value="" size="20" autocapitalize="off" /></label>
@@ -34,13 +34,13 @@
 			<p class="forgetmenot"><label for="rememberme"><input name="rememberme" type="checkbox" id="rememberme" value="forever"  /> Remember Me</label></p>
 	<p class="submit">
 		<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Log In" />
-				<input type="hidden" name="redirect_to" value="/wp-admin/" />
+				<input type="hidden" name="redirect_to" value="https://sreenathar.github.io/ckiw/wp-admin/" />
 					<input type="hidden" name="testcookie" value="1" />
 	</p>
 	</form>
 
 			<p id="nav">
-					<a href="/https://sreenathar.github.io/ckiw/wp-login.php?action=lostpassword">Lost your password?</a>
+					<a href="https://sreenathar.github.io/ckiw/wp-login.php?action=lostpassword">Lost your password?</a>
 				</p>
 	
 	<script type="text/javascript">
@@ -57,7 +57,7 @@
 			if(typeof wpOnload=='function')wpOnload();
 			</script>
 
-			<p id="backtoblog"><a href="/https://sreenathar.github.io/ckiw/">
+			<p id="backtoblog"><a href="https://sreenathar.github.io/ckiw/">
 		&larr; Back to Citizenship	</a></p>
 			
 	</div>
